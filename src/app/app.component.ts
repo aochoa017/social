5a7ca69f229196dc90437d2a68f1c398fac3680f
aitorochoa <aochoa017@ikasle.ehu.es>
2017-05-29 22:21:19 +0200

    Add jquery in an angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit(){
+    console.log("llega");
+    var button = jQuery(".button-collapse");
+    button.sideNav();
+  }
+
 }
