5b4898fb3e3c545fe70b5747bf2c431e38899aa3
aitorochoa <aochoa017@ikasle.ehu.es>
2017-06-01 21:58:54 +0200

    Menu new items added 0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var jQuery: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,13 +9,7 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   ngOnInit(){
-    console.log("llega");
-    jQuery(".button-collapse").sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    });
+
   }
 
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery: any;
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  title = 'app works!';
 
-  constructor() { }
+  isCollapsibleOpen = false;
 
-  ngOnInit() {
+  ngOnInit(){
+    console.log("llega");
+    jQuery(".button-collapse").sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+  }
+
+  onClickCollapsible(event: any){
+    console.log("onClickCollapsible");
+    this.isCollapsibleOpen = !this.isCollapsibleOpen;
+    var thisIcon = jQuery(event.target);
+    if( !thisIcon.hasClass("rotate") ){
+      thisIcon = thisIcon.find("i.rotate");
+    }
+    console.log(thisIcon);
+    if( this.isCollapsibleOpen ){
+      thisIcon.addClass("rotate-180");
+    } else {
+      thisIcon.removeClass("rotate-180");
+    }
+
   }
 
 }
