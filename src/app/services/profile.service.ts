30474b3dd201e161f2b576549e8afdfee235e6d2
aitorochoa <aochoa017@ikasle.ehu.es>
2017-06-03 00:18:31 +0200

    Profile and User service

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { Profile } from '../entities/profile';
+
+@Injectable()
+export class ProfileService {
+
+  private apiUrl = 'http://localhost:8888/api/profile';  // URL to web API
+
+  constructor( private http: Http ){}
+
+  getProfile(id: number): Observable<Profile[]> {
+    return this.http.get(this.apiUrl + '/' + id)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  putProfile(id: number, body: Object): Observable<Profile> {
+    let data = JSON.stringify(body); // Stringify payload
+    console.log(body);
+    console.log(data);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.put(this.apiUrl + '/' + id, data, options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+}
