606038d9e4faf2432dc493798ede822444e78cd6
aitorochoa <aochoa017@ikasle.ehu.es>
2017-06-03 00:19:01 +0200

    Profile and User Entities

---
+++ src/app/entities/profile.ts
@@ -0,0 +1,50 @@
+import { User } from './user';
+
+export class Profile extends User {
+
+   name: String;
+
+   surname: String;
+
+   adress: String;
+
+   city: String;
+
+   country: String;
+
+   zipCode: String;
+
+   email: String;
+
+   phone: String;
+
+   biography: String;
+
+  // constructor(
+  //   public id: String,
+  //   public name: String,
+  //   public adress: String,
+  //   public city: String,
+  //   public country: String,
+  //   public zipCode: String,
+  //   public email: String,
+  //   public phone: String,
+  //   public biography: String ) {
+  //   super(id);
+  //   // this.name = name;
+  // }
+
+  constructor(){
+    super();
+    this.name = "Aitor";
+    this.surname = "";
+    this.adress = "";
+    this.city = "";
+    this.country = "";
+    this.zipCode = "";
+    this.email = "";
+    this.phone = "";
+    this.biography = "";
+  }
+
+}
---
+++ src/app/entities/user.ts
@@ -0,0 +1,22 @@
+export class User {
+
+  // public id: String;
+
+  // public user: String;
+
+  /**
+   * Construct
+   */
+  // constructor( public id: String = "16" ) {
+  //   // this.id = id;
+  // }
+
+  id: String = "43";
+  user: String = "user43";
+
+  constructor(){
+    this.id = "23";
+    this.user = "user23";
+  }
+
+}
